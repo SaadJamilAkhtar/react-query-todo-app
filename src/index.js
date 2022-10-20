966510ac7c8571ae83a21091335bc6d90e830d7e
MuhammadSaadJamil <muhammadsaadjamil1@gmail.com>
2022-10-21 01:36:35 +0500

    added readme

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import reportWebVitals from './reportWebVitals';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
 
-
 const queryClient = new QueryClient()
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
